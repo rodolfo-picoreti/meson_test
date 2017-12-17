2679951129b416abd25c327c7248972927e091bd
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2017-12-17 13:25:03 -0200

    first commit

---
+++ helloworld.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <SimpleAmqpClient/SimpleAmqpClient.h>
+#include <google/protobuf/empty.pb.h>
+#include <google/protobuf/timestamp.pb.h>
+#include <opencv2/highgui.hpp>
+#include <boost/filesystem.hpp>
+
+int helloworld();
